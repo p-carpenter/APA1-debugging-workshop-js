42d5fb29dfb18040b9c9032eec2bf3ccce661284
p-carpenter <poppy.carpenter@ada.ac.uk>
2025-01-13 12:08:18 +0000

    fix: increment + capitalise

--- 02. increment/solution.js
+++ 02. increment/solution.js
@@ -1,10 +1,13 @@
 function increment(n) {
-  return n++;
+  return ++n;
 }
 
 module.exports = { increment };
 
 // Remember to check the expected behaviour of this program first
 // Then call the function to check how it is actually working...
-
+console.log(increment(5))
 // Write some comments down below with your findings... 
+// Doesn't increment. Returns the same number passed in.
+// n++ increments the value after it's returned (it returns the original value).
+// ++n increments it before the value is returned (returns the new incremented value.)
--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,8 +1,12 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
+  const lowercase = word.toLowerCase()
+  const firstLetter = lowercase.charAt(0).toUpperCase()
+  word = firstLetter + lowercase.slice(1)
   return word;
 }
 
+console.log(capitalise('test'))
 module.exports = { capitalise };
 
- 
+// word[0] indexes to the first letter of the string
+// 
