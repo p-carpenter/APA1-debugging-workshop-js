3e22839d3dd9e73644a2a6eb82ffeab33add7ee4
p-carpenter <poppy.carpenter@ada.ac.uk>
2025-01-13 12:56:54 +0000

    haven't finished

--- 06. 12-hour-clock/solution.js
+++ 06. 12-hour-clock/solution.js
@@ -1,11 +1,16 @@
 function convertTo12HourClock(time) {
   const hours = time.slice(0, 2);
+  console.log(hours)
+  console.log(hours - 12)
+  // console.log(hours)
   const minutes = time.slice(-2);
-  if (hours > 12) {
-    return `${hours - 12}:${minutes} PM`;
-  } else {
+  console.log(minutes)
+  if (hours >= 10) {
+    return `${hours}:${minutes} PM`;
+  } else if (hours >= 0) {
+    // console.log(typeof hours)
     return `${hours}:${minutes} AM`;
   }
 }
-
+console.log(convertTo12HourClock("00:00"))
 module.exports = { convertTo12HourClock };
